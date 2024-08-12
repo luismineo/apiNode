68c6b1017c079261dea17511658ff16cff60d5c5
Luis Mineo <luisafonsomineo@gmail.com>
2024-08-12 20:25:14 -0300

    aula dia 12

---
+++ config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'admin',
+    password: 'admin',
+    database: 'apinode',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+};
